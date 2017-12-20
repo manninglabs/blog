0750ce742cfe1852adbc80e669bc0fe2a9f2c995
Лаптев Артем <artemlaptev63@mail.ru>
2017-12-20 15:10:14 +0300

    added disqus

--- src/TutorialDetail.js
+++ src/TutorialDetail.js
@@ -3,8 +3,13 @@ import './TutorialDetail.css';
 import posts from './posts';
 import ReactMarkdown from 'react-markdown';
 import mixpanel from 'mixpanel-browser';
+import ReactDisqusThread from 'react-disqus-thread';
 
 class TutorialDetail extends Component {
+
+  handleNewComment(comment) {
+    console.log(comment.text());
+  }
 
   render() {
     mixpanel.init('831c108f9cb2220b0e431bbb7f97493c');
@@ -20,6 +25,13 @@ class TutorialDetail extends Component {
       <div>
         <h1>{ post.title }</h1>
         <ReactMarkdown source={ post.content } />
+        <ReactDisqusThread
+                shortname="example"
+                identifier="something-unique-12345"
+                title="Example Thread"
+                url="http://www.example.com/example-thread"
+                category_id="123456"
+                onNewComment={this.handleNewComment}/>
       </div>
     );
   }
